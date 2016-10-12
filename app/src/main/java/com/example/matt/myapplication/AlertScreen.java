152576462bfe5ff8261d053ef5ee17e99022900a
matt <krazyasian@live.com.au>
2016-10-12 10:41:49 +1030

    initial commit

---
+++ app/src/main/java/com/example/matt/myapplication/AlertScreen.java
@@ -0,0 +1,13 @@
+package com.example.matt.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AlertScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alert_screen);
+    }
+}
